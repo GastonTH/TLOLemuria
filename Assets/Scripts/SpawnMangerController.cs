8a8addf2a236a346b4a09f4e4a876d70ba840ea0
Gaston Tomas Huete <gaston.tomas.huete.1997@gmail.com>
2022-05-27 14:04:08 +0200

    spawnpoint

---
+++ Assets/Scripts/SpawnMangerController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class SpawnMangerController : MonoBehaviour
+{
+    public List<SpawnPoint> spawnPoints;
+}
